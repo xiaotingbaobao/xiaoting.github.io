5060bc61212ac3d8f5ceef5eb489df78095f49e9
ting <924547237@qq.com>
2019-06-25 20:06:04 +0800

    个人简历

---
+++ personal resume/src/components/home/Home.vue
@@ -0,0 +1,279 @@
+<template>
+  <div class="content col-md-12">
+    <div class="Header">
+      <div class="header_img">
+        <img src="../../assets/img/photo.png">
+      </div>
+    </div>
+    <div class="Intro">
+      <p class="intro_text1">吴婷</p>
+      <p class="intro_text2">个人简历</p>
+    </div>
+    <div class="Details col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+      <div class="details_person">
+        <h1 class="style_h1">个人信息</h1>
+        <div  class="list_content">
+        <ul class="nav nav_list">
+          <li class="col-md-4 list_h1">●&nbsp;吴婷&nbsp;|&nbsp;女&nbsp;|&nbsp;1995</li>
+          <li class="col-md-4 list_h2">●&nbsp;鄂东&nbsp;|&nbsp;大专&nbsp;|&nbsp;电子商务</li>
+          <li class="col-md-4 list_h3">●&nbsp;前端开发&nbsp;|&nbsp;1年工作经验</li>
+        </ul>
+        </div>
+      </div>
+      <div class="details_phone">
+        <h1 class="style_h1">联系方式</h1>
+        <div  class="list_content">
+          <ul class="nav nav_list">
+            <li class="col-md-4 list_h1">●&nbsp;手机:&nbsp;15527627141</li>
+            <li class="col-md-4 list_h2">●&nbsp;Email:&nbsp;924547237@qq.com</li>
+            <li class="col-md-4 list_h3">●&nbsp;微信:&nbsp;15527627141</li>
+          </ul>
+        </div>
+      </div>
+      <div class="details_post">
+        <h1 class="style_h1">求职意向</h1>
+        <div  class="post_content">
+          <ul class="nav nav_list">
+            <li class="list_s1">●&nbsp;期望工作性质：全职</li>
+            <li class="list_s2">●&nbsp;期望从事职业：前端工程师</li>
+            <li class="list_s3">●&nbsp;期望月薪：面议</li>
+            <li class="list_s4">●&nbsp;目前状况：求职</li>
+          </ul>
+        </div>
+      </div>
+      <div class="details_skill">
+        <h1 class="style_h1">技能及自我评价</h1>
+        <div  class="skill_content">
+          <ul class="nav nav-list">
+            <li class="list_s1">●&nbsp;熟练掌握前端相关技术（HTML5/Css3/Javascript ）开发，使用合理的结构和样式编写页面.</li>
+            <li class="list_s1">●&nbsp;熟悉原生javascript ，作用域，了解作用域以及闭包，原型、ajax等基础概念，以及jQuery。</li>
+            <li class="list_s1">●&nbsp;熟悉Vue框架，了解es6，了解GIT版本控制工具和webpack打包工具。</li>
+            <li class="list_s1">●&nbsp;熟悉Bootstrap进行响应式页面开发。</li>
+            <li class="list_s1">●&nbsp;熟悉现代css预处理器less，sass，了解scss。</li>
+            <li class="list_s1">●&nbsp;了解微信小程序开发。</li>
+            <li class="list_s1">●&nbsp;了解canvas动画制作。</li>
+            <li class="list_s1">●&nbsp;熟悉Photoshop，把设计的图片按照需要交互的要求进行效果实现。</li>
+            <div class="list_my"><span>自我评价：</span>
+              有良好的代码习惯，能根据设计稿对页面进行95%以上的布局还原制作。热爱互联网行业，有良好的代码编程习惯，较强的逻辑思维能力。自学能力强，有较好的团队协作和沟通能力，有强烈的责任心。</div>
+          </ul>
+        </div>
+      </div>
+      <div class="details_post">
+        <h1 class="style_h1">作品集</h1>
+        <div  class="post_content">
+          <ul class="nav nav_list">
+            <li class="list_s1">●&nbsp;<span><a href="https://xiaotingzi.shop/tianmao/">仿天猫网站：https://xiaotingzi.shop/tianmao/</a></span> （注：原生js仿天猫首页）</li>
+            <li class="list_s1">●&nbsp;<span><a href="https://xiaotingzi.shop/game-mine sweeping/">踩地雷小游戏 ：https://xiaotingzi.shop/game-mine sweeping/</a></span>（注：用原生js写成的小游戏）</li>
+            <li class="list_s1">●&nbsp;<span><a href="https://xiaotingzi.shop/bootstrap/">Bootstrap网站 ：https://xiaotingzi.shop/bootstrap/</a></span>（注： 用bootstrap写出的网站）</li>
+            <li class="list_s1">●&nbsp;<span><a href="https://xiaotingzi.shop/translate/">Translate翻译工具：https://xiaotingzi.shop/translate/</a></span>（注： 用vue写出的小工具）</li>
+            <li class="list_s4">●&nbsp;<span><a href="https://github.com/xiaotingbaobao">GitHub地址：https://github.com/xiaotingbaobao</a></span></li>
+          </ul>
+        </div>
+      </div>
+      <div class="details_work">
+        <h1 class="style_h1">工作经历</h1>
+        <div  class="work_content">
+          <div class="work_content_name">
+            ●掌通宝网络科技有限公司（2018/4-2019/3）
+          </div>
+          <div class="work_content_details nav nav-list">
+            <div class="work_content_p">
+              <div class="work_span1">项目描述：</div>
+              <div class="work_span2">
+                项目是一款教育类软件，分为三个系统，手机版，门户和后台管理平台。教师可以在教师版app或门户上发布通知作业等，家长在家长版app上即可收到相应的消息，另外app上会有其他板块，例如聊天平台、新闻板块、诗词游戏、举报投诉建议和视频等。后台管理则是管理用户信息，处理用户投诉举报等。
+              </div>
+            </div>
+            <div class="work_content_p">
+              <div class="work_span1">负责板块：</div>
+              <div class="work_span2">
+                根据任务按照设计师的要求切图后编写相应页面，实现产品UI和交互方面的开发需求，确保产品具有优质的用户使用体验。项目中我独立负责的有新闻板块、投诉建议，新闻板块中后台人员上传后将在新闻板块展现。其余部分板块参与部分。
+              </div>
+            </div>
+            <div class="work_content_p1">
+              <div class="work_span1">技术描述：</div>
+              <div class="work_span2">
+                1、使用HTML5、Css3编写页面，通过优雅降级，处理各种浏览器兼容性问题。<br/>
+                2、使用javascript实现页面中需要效果，运用jQuery框架库，提高团队开发效率。<br/>
+                3、使用swiper做页面滑屏效果，使用icon阿里矢量图标库。<br/>
+                4、使用flex弹性布局实现手机端页面响应式，利用rem布局适配不同的移动终端,完成响应式布局；<br/>
+                5、通过Ajax + Jsonp 与后台数据进行交互，与后端工作有效对接，优化网站前端性。<br/>
+                6、优化：sprite图，图片懒加载，将重复使用的css样式剥离出来做成公用样式，对常用的轮播、Tab切换等进行封装，用css3代替js来完成动画，压缩css、js及图片。<br/>
+                7、项目中会用到webpack的依赖模块理和打包。<br/>
+                8、团队讨论具体功能的细节问题，与其他技术人员沟通并制定设计规范。<br/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="details_over">感谢您花时间阅读我的简历， 期待能有机会和您共事。🌼</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style>
+  .content{
+    background: url("../../assets/img/bg.jpg") repeat;
+  }
+  .Header{
+    width: 100%;
+    height: 3.4rem;
+    overflow: hidden;
+  }
+  .header_img{
+    width: 2.4rem;
+    height: 2.4rem;
+    border-radius: 50%;
+    overflow: hidden;
+    margin: 1rem auto 0 auto;
+  }
+  .Header img{
+    width: 100%;
+    height: 100%;
+  }
+  .Intro{
+    width: 100%;
+    margin-top: .72rem;
+  }
+  .intro_text1{
+    margin: 0 auto;
+    text-align: center;
+    width: 2rem;
+    height: .86rem;
+    line-height: .86rem;
+    background: #f7f3ee;
+    color: #948c76;
+    border-radius: .1rem;
+    font-size: .38rem;
+  }
+  .intro_text2{
+    margin: .72rem auto 0 auto;
+    text-align: center;
+    width: 2rem;
+    height: .6rem;
+    line-height: .6rem;
+    font-size: .28rem;
+    color: #948c76;
+  }
+  .Details{
+    padding: 20px;
+    box-shadow: 1px 1px 8px rgba(0,0,0,.15);
+    background-color: #fff;
+    color: #3f1c1c;
+    font-size: 14px;
+    margin-top: 15px;
+    margin-bottom: 20px;
+  }
+  .details_person{}
+  .style_h1{
+    font-size: .36rem;
+    font-weight: bold;
+    height: .5rem;
+    line-height: .5rem;
+    color:#111;
+    margin-bottom: 20px;
+    border-left: 5px solid #e38fa9;
+    margin-left: -20px;
+    padding-left: 15px;
+    margin-top: 20px;
+  }
+  .list_content{
+    width: 100%;
+    border: 2px solid #ddd;
+    background-color: #f4f4f4;
+    border-radius: 5px;
+  }
+  .list_h1 ,.list_h2{
+    height: .78rem;
+    line-height: .78rem;
+    border-right: 2px solid #ddd;
+  }
+  .list_h3{
+    height: .78rem;
+    line-height: .78rem;
+  }
+  .post_content{
+    width: 100%;
+    border: 2px solid #ddd;
+    background-color: #f4f4f4;
+    border-radius: 5px;
+    font-size: .28rem;
+  }
+  .post_content a{
+    list-style: none;
+    color: #000;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+  .post_content a:hover{
+    color: #e38fa6;
+    font-size: .3rem;
+  }
+  .list_s1 ,.list_s2 , .list_s3{
+    padding: .18rem 0 .14rem 15px;
+    border-bottom: 2px solid #ddd;
+    line-height: .49rem;
+  }
+  .list_my{
+    padding: .18rem 0 .14rem 15px;
+    line-height: .49rem;
+  }
+  .list_s4{
+    padding: .18rem 0 .14rem 15px;
+    line-height: .49rem;
+  }
+  .list_my span{
+    font-weight: bold;
+  }
+  .skill_content{
+    width: 100%;
+    border: 2px solid #ddd;
+    background-color: #f4f4f4;
+    border-radius: 5px;
+    font-size: .28rem;
+  }
+  .work_content{
+    width: 100%;
+    border: 2px solid #ddd;
+    background-color: #f4f4f4;
+    border-radius: 5px;
+    font-size: .28rem;
+  }
+  .work_content_name{
+    height: .78rem;
+    line-height: .78rem;
+    padding-left: 15px;
+    border-bottom: 2px solid #ddd;
+  }
+  .work_content_p{
+    display: flex;
+    padding-left: 15px;
+    line-height: .5rem;
+  }
+  .work_content_p1{
+    display: flex;
+    padding-left: 15px;
+    line-height: .5rem;
+    margin-bottom: .3rem;
+  }
+  .work_span1{
+    width: 70px;
+    margin-top: .2rem;
+    color: #e38fa9;
+
+  }
+  .work_span2{
+    flex: 1;
+    margin-top: .2rem;
+    line-height: .5rem;
+  }
+  .details_over{
+    margin: .4rem 0 .1rem .3rem;
+    font-size: .3rem;
+    color: #e38fa9;
+  }
+</style>
